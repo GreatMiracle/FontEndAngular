ec48fcf147de7b1f7403b00cc2b9d346e029e1f7
KienTienSinh <kiennguyen6868@outlook.com>
2021-02-27 18:43:52 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,40 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './component/product-list/product-list.component';
+import { SearchComponent } from './component/search/search.component';
+import { ProductDetailsComponent } from './component/product-details/product-details.component';
+import { ProductCategoryMenuComponent } from './component/product-category-menu/product-category-menu.component';
+import {RouterModule, Routes} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import {ProductService} from './service/product.service';
+
+
+const routes: Routes = [
+  {path: 'products/:id', component: ProductDetailsComponent},
+  {path: 'search/:keyword', component: ProductListComponent},
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    SearchComponent,
+    ProductDetailsComponent,
+    ProductCategoryMenuComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/component/product-details/product-details.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import {Product} from '../../common/product';
+import {ProductService} from '../../service/product.service';
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+
+
+  product: Product = new Product();
+
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      this.handleProductDetails();
+    });
+  }
+
+  // tslint:disable-next-line:typedef
+  handleProductDetails() {
+    // get the "id" param string. convert string to a number using the "+" symbol
+    const theProductId: number = +this.route.snapshot.paramMap.get('id');
+
+    this.productService.getProduct(theProductId).subscribe(
+      data => {
+        this.product = data;
+      }
+    );
+  }
+
+}
